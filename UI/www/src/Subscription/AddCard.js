f53ea9b3b908162c8044169907a8c676c7927ca0
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-01-24 16:50:14 +0000

    added call to API

--- UI/www/src/Subscription/AddCard.js
+++ UI/www/src/Subscription/AddCard.js
@@ -22,6 +22,25 @@ class AddCard extends Component {
                 // show success message and navigate away from form
                 console.log('Response: ' + JSON.stringify(response))
                 console.log('Need to send card token to server: ' + cardToken)
+
+                var params = {
+                    //This is where any header, path, or querystring request params go. The key is the parameter named as defined in the API
+                };
+                // Template syntax follows url-template https://www.npmjs.com/package/url-template
+                var pathTemplate = '/v1/subscription/addCard'
+                var method = 'POST';
+                var additionalParams = {};
+                var body = {
+                    token: cardToken
+                };
+
+                apigClient.invokeApi(params, pathTemplate, method, additionalParams, body)
+                    .then(function (result) {
+                        console.log('Card set for user')
+                    }).catch(function (result) {
+                    //This is where you would put an error callback
+                        console.log('Card not set for user')
+                });
             }
         });
     }
