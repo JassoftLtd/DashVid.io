8eb5b6995f3bfcab9c9e8b509ea1d58b41ac91d6
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-01-27 22:15:17 +0000

    setting default variables

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -69,22 +69,22 @@ resource "aws_s3_bucket" "dash-cam-videos-premium-bucket" {
 }
 
 // Trigger UploadedVideo Lambda when ObjectCreated
-resource "aws_s3_bucket_notification" "free_bucket_created_notification" {
-    depends_on = ["aws_s3_bucket.dash-cam-videos-free-bucket"]
-    bucket = "${aws_s3_bucket.dash-cam-videos-free-bucket.id}"
-    lambda_function {
-        lambda_function_arn = "${aws_lambda_function.uploadedVideo.arn}"
-        events = ["s3:ObjectCreated:*"]
-    }
-}
-resource "aws_s3_bucket_notification" "standard_bucket_created_notification" {
-    depends_on = ["aws_s3_bucket.dash-cam-videos-standard-bucket"]
-    bucket = "${aws_s3_bucket.dash-cam-videos-standard-bucket.id}"
-    lambda_function {
-        lambda_function_arn = "${aws_lambda_function.uploadedVideo.arn}"
-        events = ["s3:ObjectCreated:*"]
-    }
-}
+//resource "aws_s3_bucket_notification" "free_bucket_created_notification" {
+//    depends_on = ["aws_s3_bucket.dash-cam-videos-free-bucket"]
+//    bucket = "${aws_s3_bucket.dash-cam-videos-free-bucket.id}"
+//    lambda_function {
+//        lambda_function_arn = "${aws_lambda_function.uploadedVideo.arn}"
+//        events = ["s3:ObjectCreated:*"]
+//    }
+//}
+//resource "aws_s3_bucket_notification" "standard_bucket_created_notification" {
+//    depends_on = ["aws_s3_bucket.dash-cam-videos-standard-bucket"]
+//    bucket = "${aws_s3_bucket.dash-cam-videos-standard-bucket.id}"
+//    lambda_function {
+//        lambda_function_arn = "${aws_lambda_function.uploadedVideo.arn}"
+//        events = ["s3:ObjectCreated:*"]
+//    }
+//}
 
 // Trigger expiredVideo Lambda when ObjectRemoved
 //resource "aws_s3_bucket_notification" "free_bucket_removed_notification" {
