0c282fa22a4ae0f1d95c10db22629a74d0803b1c
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-02-02 19:13:52 +0000

    added asserts to tests

--- concourse/test/api-test/test/testAuth.js
+++ concourse/test/api-test/test/testAuth.js
@@ -17,22 +17,45 @@ describe('Auth', function () {
             var password = generator.password()
 
             return authHelper.signup(email, password, "Free")
+                .then(function (result) {
+                    assert.equal(result.data.created, true);
+                });
 
         })
     });
 
     describe('Account Verification', function () {
 
-        it('Should allow me to activate a new account', function () {
+        it('Given I activate a new account, When signing up for the Free plan, Then the returned status should be Active', function () {
 
             var email = generator.email()
             var password = generator.password()
 
-            authHelper.signup(email, password, "Free")
+            return authHelper.signup(email, password, "Free")
                 .then(function (result) {
                     return authHelper.verify(email, tokenOverride)
+                        .then(function (result) {
+                            assert.equal(result.data.verified, true);
+                            assert.equal(result.data.plan, "Free");
+                            assert.equal(result.data.status, "Active");
+                        });
                 });
+        })
 
+        it('Given I activate a new account, When signing up for the Standard plan, Then the returned status should be Pending', function () {
+
+            var email = generator.email()
+            var password = generator.password()
+
+            return authHelper.signup(email, password, "Standard")
+                .then(function (result) {
+                    return authHelper.verify(email, tokenOverride)
+                        .then(function (result) {
+                            assert.equal(result.data.verified, true);
+                            assert.equal(result.data.plan, "Standard");
+                            assert.equal(result.data.status, "Pending");
+                        });
+                });
         })
     });
 
@@ -43,11 +66,16 @@ describe('Auth', function () {
             var email = generator.email()
             var password = generator.password()
 
-            authHelper.signup(email, password, "Free")
+            return authHelper.signup(email, password, "Free")
                 .then(function (result) {
-                    authHelper.verify(email, tokenOverride)
+                    return authHelper.verify(email, tokenOverride)
                         .then(function (result) {
                             return authHelper.login(email, password)
+                                .then(function (result) {
+                                    assert.equal(result.data.login, true);
+                                    // assert.equal(result.data.identityId, "");
+                                    // assert.equal(result.data.token, "");
+                                });
                         });
                 });
 
