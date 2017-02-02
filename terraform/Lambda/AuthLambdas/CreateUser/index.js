60d06e4f849eb95e99a64b9f377022c23e8da2e3
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-02-02 19:27:48 +0000

    updated logging in signup

--- terraform/Lambda/AuthLambdas/CreateUser/index.js
+++ terraform/Lambda/AuthLambdas/CreateUser/index.js
@@ -52,6 +52,8 @@ function storeUser(event, email, hash, salt, fn) {
 		if (err) return fn(err);
 		token = token.toString('hex');
 
+
+        console.log('Auth token override: ' + process.env.token_override)
         if(process.env.token_override && process.env.token_override !== '') {
             console.log('Auth token override: ' + process.env.token_override)
             token = process.env.token_override
