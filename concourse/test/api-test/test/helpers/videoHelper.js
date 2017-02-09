a45eb3a24ac046c12c553e7069b2b6193b560268
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-02-09 15:23:53 +0000

    added get videos test

--- concourse/test/api-test/test/helpers/videoHelper.js
+++ concourse/test/api-test/test/helpers/videoHelper.js
@@ -25,3 +25,25 @@ exports.createVideo  = function (user, fileName, fileType) {
 
     return apigClient.invokeApi(params, pathTemplate, method, additionalParams, body)
 }
+
+exports.getVideos  = function (user) {
+    var config = {
+        invokeUrl: process.env.DASHVID_API_ADDRESS,
+        accessKey: user.credentials.accessKeyId,
+        secretKey: user.credentials.secretAccessKey,
+        sessionToken: user.credentials.sessionToken
+    }
+
+    var apigClient = apigClientFactory.newClient(config);
+
+    var params = {
+        //This is where any header, path, or querystring request params go. The key is the parameter named as defined in the API
+    };
+    // Template syntax follows url-template https://www.npmjs.com/package/url-template
+    var pathTemplate = '/v1/video'
+    var method = 'GET';
+    var additionalParams = {};
+    var body = {};
+
+    return apigClient.invokeApi(params, pathTemplate, method, additionalParams, body)
+}
--- concourse/test/api-test/test/testVideo.js
+++ concourse/test/api-test/test/testVideo.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 var fs = require('fs');
 var request = require('request');
 var rp = require('request-promise');
+var sleep = require('sleep');
 
 var authHelper = require('./helpers/authHelper.js');
 var videoHelper = require('./helpers/videoHelper.js');
@@ -41,6 +42,64 @@ describe('Video', function () {
                             return rp(options)
                                 .then(function () {
                                     console.log('Uploaded Video')
+                                });
+
+                        });
+
+                });
+        });
+    });
+
+
+    describe('Get Videos', function () {
+
+        it('Given I have a verified account, When I havent uploaded any videos, Then It should recieve an empty list', function () {
+           return authHelper.getLoggedInUser()
+                .then(function (user) {
+
+                    return videoHelper.getVideos(user)
+                        .then(function (result) {
+                            assert.equal(result.data.videos.length, 0);
+                        })
+
+                });
+        });
+
+        it('Given I have a verified account, When I request to upload a video, Then It should be listed', function () {
+           return authHelper.getLoggedInUser()
+                .then(function (user) {
+
+                    return videoHelper.createVideo(user, "01291238_0160.MP4", ".MP4")
+                        .then(function (result) {
+                            assert(result.data.url);
+
+                            console.log('Uploading file')
+
+                            let filepath = './test/testData/01291238_0160.MP4';
+
+                            let stream = fs.createReadStream(filepath)
+                            let stat = fs.statSync(filepath);
+
+                            let options = {
+                                method: 'PUT',
+                                uri: result.data.url,
+                                body: stream,
+                                headers: {
+                                    'content-type': 'text/plain;charset=UTF-8',
+                                    'Content-Length': stat.size
+                                }
+                            };
+
+                            return rp(options)
+                                .then(function () {
+                                    console.log('Uploaded Video')
+
+                                    sleep.sleep(2)
+
+                                    return videoHelper.getVideos(user)
+                                        .then(function (result) {
+                                            assert.equal(result.data.videos.length, 1);
+                                        })
                                 });
 
                         });
