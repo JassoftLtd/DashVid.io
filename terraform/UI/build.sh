903a42fa70ddce55d488285fd12c4c0d5b1f478c
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-02-15 19:10:05 +0000

    fixing ui build

--- terraform/UI/build.sh
+++ terraform/UI/build.sh
@@ -22,4 +22,4 @@ cd ../
 
 websiteBucket=dashvid.io
 
-$GOPATH/bin/terraform-s3-dir ./UI/www/build/ $TF_VAR_environment_name$websiteBucket aws_s3_bucket.dashvid-io-bucket > s3_dashvid-io.tf
+$GOPATH/bin/terraform-s3-dir ./www/build/ $TF_VAR_environment_name$websiteBucket aws_s3_bucket.dashvid-io-bucket > s3_dashvid-io.tf
