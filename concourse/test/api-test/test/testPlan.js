c5b9691469f1cf2286ee3119bf0b8df82a9c63f3
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-02-28 10:20:02 +0000

    added plan test

---
+++ concourse/test/api-test/test/testPlan.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+var fs = require('fs');
+var request = require('request');
+var rp = require('request-promise');
+var sleep = require('sleep');
+
+var authHelper = require('./helpers/authHelper.js');
+var planHelper = require('./helpers/planHelper.js');
+var generator = require('./helpers/generators.js');
+
+describe('Plan', function () {
+
+    this.timeout(60000);
+
+    describe('Get Plan', function () {
+
+        it('Given I have a verified account, When I request my plan, Then I should be given name of my current Plan', function () {
+           return authHelper.getLoggedInUser()
+                .then(function (user) {
+
+                    return planHelper.getPlan(user)
+                        .then(function (result) {
+                            assert(result.data.plan);
+                            assert.equal(result.data.plan, "Free");
+                        });
+
+                });
+        });
+    });
+
+});
