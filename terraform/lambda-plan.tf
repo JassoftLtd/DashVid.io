87e7905aa433596db984e96398c53751fdb28fac
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-03-08 14:46:09 +0000

    modify source arn

--- terraform/lambda-plan.tf
+++ terraform/lambda-plan.tf
@@ -16,5 +16,5 @@ resource "aws_lambda_permission" "allow_api_gateway-getPlan" {
   statement_id = "AllowGetPlanExecutionFromApiGateway"
   action = "lambda:InvokeFunction"
   principal = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_id}:${aws_api_gateway_rest_api.DashCamAPI.id}/*/${aws_api_gateway_method.Plan-GET.http_method}${aws_api_gateway_resource.Plan.path}"
+  source_arn = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_id}:${aws_api_gateway_rest_api.DashCamAPI.id}/*/${aws_api_gateway_method.Plan-GET.http_method}/v1/${aws_api_gateway_resource.Plan.path_part}"
 }
