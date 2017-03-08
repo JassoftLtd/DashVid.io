bdb0367479901045ce1f4a4581b4fc6c9d52a2f2
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-03-08 22:29:28 +0000

    more refactoring

--- terraform/lambda-plan.tf
+++ terraform/lambda-plan.tf
@@ -10,11 +10,3 @@ resource "aws_lambda_function" "getPlan" {
   timeout = "30"
   source_code_hash = "${base64sha256(file("Lambda/PlanLambdas/getPlan.zip"))}"
 }
-
-resource "aws_lambda_permission" "allow_api_gateway-getPlan" {
-  function_name = "${aws_lambda_function.getPlan.function_name}"
-  statement_id = "AllowGetPlanExecutionFromApiGateway"
-  action = "lambda:InvokeFunction"
-  principal = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_id}:${aws_api_gateway_rest_api.DashCamAPI.id}/*/${aws_api_gateway_method.Plan-GET.http_method}/v1/${aws_api_gateway_resource.Plan.path_part}"
-}
