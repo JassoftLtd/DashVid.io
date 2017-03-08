216ab531088cd72eb9df00c8d6c46ecbaedb52e8
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-03-08 20:53:44 +0000

    remove depends on

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -56,7 +56,6 @@ resource "aws_api_gateway_deployment" "DevDeployment" {
   depends_on = [
     "aws_api_gateway_rest_api.DashCamAPI",
     "aws_api_gateway_integration_response.resetPassword-POST-Integration-Response",
-    "ApiGatewayLambda-addCard",
     "aws_api_gateway_integration_response.Video-POST-Integration-Response"
   ]
   rest_api_id = "${aws_api_gateway_rest_api.DashCamAPI.id}"
