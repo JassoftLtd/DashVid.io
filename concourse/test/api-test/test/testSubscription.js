0731feb3126693af28b2298d25a19abe0e8066c1
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-03-15 19:00:29 +0000

    fixed add card test

--- concourse/test/api-test/test/testSubscription.js
+++ concourse/test/api-test/test/testSubscription.js
@@ -4,10 +4,8 @@ var authHelper = require('./helpers/authHelper.js');
 var subscriptionHelper = require('./helpers/subscriptionHelper.js');
 var generator = require('./helpers/generators.js');
 
-var stripeAsPromised = require('stripe-as-promised')
 
-var stripe_api = require('stripe-api')('pk_test_ebVZiJokoWIbXD1TNNZ8lj2A');
-var stripe = stripeAsPromised(stripe_api, Promise);
+var stripe = require('stripe')('pk_test_ebVZiJokoWIbXD1TNNZ8lj2A');
 
 describe('Subscription', function () {
 
@@ -15,26 +13,28 @@ describe('Subscription', function () {
 
     describe('Add Card', function () {
 
-        it('Given I have a verified account on a pending standard plan, When I add a card, Then my plan should become active', function () {
-           return authHelper.getLoggedInUser("standard")
+        it('Given I have a verified account on a pending standard plan, When I add a card, Then my plan should become active', function (done) {
+           authHelper.getLoggedInUser("standard")
                 .then(function (user) {
 
-                    var card = {
-                        number: '4242424242424242',
-                        cvc: '123',
-                        exp_month: '04',
-                        exp_year: '20',
-                        address_zip: ''
+                    var payload = {
+                        card: {
+                            number: '4242424242424242',
+                            cvc: '123',
+                            exp_month: '04',
+                            exp_year: '20'
+                        }
                     }
 
-                    return stripe.card.createToken(card)
-                        .then(function (token) {
 
-                            return subscriptionHelper.addCard(user, generator.cardToken())
-                                .catch(function (error) {
-                                    console.error(error)
-                                })
-                        });
+                    stripe.tokens.create(payload, function (err, token) {
+
+                        subscriptionHelper.addCard(user, token.id)
+                            .then(function (result) {
+                                assert.equal(result.data.added, true);
+                                done();
+                            })
+                    })
 
                 });
         });
