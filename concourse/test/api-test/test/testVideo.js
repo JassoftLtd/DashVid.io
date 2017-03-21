bdbb8d1c11ef393d11b40190688f8248ac241d6f
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-03-21 20:50:58 +0000

    enable commented video tests

--- concourse/test/api-test/test/testVideo.js
+++ concourse/test/api-test/test/testVideo.js
@@ -48,61 +48,58 @@ describe('Video', function () {
         });
     });
 
-    // describe('Get Videos', function () {
-    //
-    //     it('Given I have a verified account, When I havent uploaded any videos, Then It should recieve an empty list', function () {
-    //        return authHelper.getLoggedInUser()
-    //             .then(function (user) {
-    //
-    //                 return videoHelper.getVideos(user)
-    //                     .then(function (result) {
-    //                         assert.equal(result.data.videos.length, 0);
-    //                     })
-    //                     .catch(function (error) {
-    //                         console.error(error)
-    //                     })
-    //
-    //             });
-    //     });
+    describe('Get Videos', function () {
 
-        // it('Given I have a verified account, When I request to upload a video, Then It should be listed', function () {
-        //    return authHelper.getLoggedInUser()
-        //         .then(function (user) {
-        //
-        //             return videoHelper.createVideo(user, "01291238_0160.MP4", ".MP4")
-        //                 .then(function (result) {
-        //                     assert(result.data.url);
-        //
-        //                     let filepath = './test/testData/01291238_0160.MP4';
-        //
-        //                     let stream = fs.createReadStream(filepath)
-        //                     let stat = fs.statSync(filepath);
-        //
-        //                     let options = {
-        //                         method: 'PUT',
-        //                         uri: result.data.url,
-        //                         body: stream,
-        //                         headers: {
-        //                             'content-type': 'text/plain;charset=UTF-8',
-        //                             'Content-Length': stat.size
-        //                         }
-        //                     };
-        //
-        //                     return rp(options)
-        //                         .then(function () {
-        //
-        //                             sleep.sleep(2)
-        //
-        //                             return videoHelper.getVideos(user)
-        //                                 .then(function (result) {
-        //                                     assert.equal(result.data.videos.length, 1);
-        //                                 })
-        //                         });
-        //
-        //                 });
-        //
-        //         });
-        // });
-    // });
+        it('Given I have a verified account, When I havent uploaded any videos, Then It should recieve an empty list', function () {
+           return authHelper.getLoggedInUser()
+                .then(function (user) {
+
+                    return videoHelper.getVideos(user)
+                        .then(function (result) {
+                            assert.equal(result.data.videos.length, 0);
+                        })
+
+                });
+        });
+
+        it('Given I have a verified account, When I request to upload a video, Then It should be listed', function () {
+           return authHelper.getLoggedInUser()
+                .then(function (user) {
+
+                    return videoHelper.createVideo(user, "01291238_0160.MP4", ".MP4")
+                        .then(function (result) {
+                            assert(result.data.url);
+
+                            let filepath = './test/testData/01291238_0160.MP4';
+
+                            let stream = fs.createReadStream(filepath)
+                            let stat = fs.statSync(filepath);
+
+                            let options = {
+                                method: 'PUT',
+                                uri: result.data.url,
+                                body: stream,
+                                headers: {
+                                    'content-type': 'text/plain;charset=UTF-8',
+                                    'Content-Length': stat.size
+                                }
+                            };
+
+                            return rp(options)
+                                .then(function () {
+
+                                    sleep.sleep(2)
+
+                                    return videoHelper.getVideos(user)
+                                        .then(function (result) {
+                                            assert.equal(result.data.videos.length, 1);
+                                        })
+                                });
+
+                        });
+
+                });
+        });
+    });
 
 });
