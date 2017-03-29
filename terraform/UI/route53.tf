d576c07f2931be06a99a661f407c7225bbd8fd91
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-03-29 21:14:40 +0100

    add route53 alias

--- terraform/UI/route53.tf
+++ terraform/UI/route53.tf
@@ -3,7 +3,6 @@ resource "aws_route53_record" "DashCamWeb" {
   name    = "${var.environment_subdomain}${var.dns_zone_name}"
   type    = "${var.route53_type}"
   ttl     = "60"
-  records = ["${aws_s3_bucket.dashvid-io-bucket.website_endpoint}"]
   alias {
     name                   = "${aws_cloudfront_distribution.website_s3_distribution.domain_name}"
     zone_id                = "${aws_cloudfront_distribution.website_s3_distribution.hosted_zone_id}"
