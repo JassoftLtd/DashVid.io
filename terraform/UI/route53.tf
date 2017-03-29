8216f64fd9085260cd26b8189481c1fc6497079e
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-03-29 20:48:43 +0100

    route53 type by variable

--- terraform/UI/route53.tf
+++ terraform/UI/route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "DashCamWeb" {
   zone_id = "${var.dns_zone_id}"
   name    = "${var.environment_subdomain}${var.dns_zone_name}"
-  type    = "CNAME"
+  type    = "${var.route53_type}"
   ttl     = "60"
   records = ["${aws_s3_bucket.dashvid-io-bucket.website_endpoint}"]
 }
