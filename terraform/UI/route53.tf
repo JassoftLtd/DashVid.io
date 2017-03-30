cb12e8ce9351e86d7f96af2dd0b1673e9d56d6ce
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-03-30 15:28:45 +0100

    added www route53 record

--- terraform/UI/route53.tf
+++ terraform/UI/route53.tf
@@ -8,3 +8,14 @@ resource "aws_route53_record" "DashCamWeb" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "www_DashCamWeb" {
+  zone_id = "${var.dns_zone_id}"
+  name    = "www.${var.environment_subdomain}${var.dns_zone_name}"
+  type    = "A"
+  alias {
+    name                   = "${aws_cloudfront_distribution.website_s3_distribution.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.website_s3_distribution.hosted_zone_id}"
+    evaluate_target_health = true
+  }
+}
