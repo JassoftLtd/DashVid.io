bb5298548a555beffa156e9ddbd891bd8a683c7a
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-04-05 12:22:13 +0100

    Print out message

--- terraform/Lambda/TranscodingLambdas/videoTranscoded/index.js
+++ terraform/Lambda/TranscodingLambdas/videoTranscoded/index.js
@@ -4,4 +4,13 @@ exports.handler = function(event, context) {
     "use strict";
 
     console.log("Event: " + JSON.stringify(event));
+
+    for(var i = 0; i < event.Records.length; i++) {
+
+        let record = event.Records[i];
+
+        let message = JSON.stringify(record.Sns.Message);
+
+        console.log("Message: " + JSON.stringify(message));
+    }
 };
