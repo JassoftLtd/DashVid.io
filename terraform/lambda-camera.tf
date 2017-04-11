a927673346b10881ef0e8f8c8494fe95af0845d6
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-04-11 22:16:50 +0100

    Use correct lambda permissions

--- terraform/lambda-camera.tf
+++ terraform/lambda-camera.tf
@@ -4,7 +4,7 @@
 resource "aws_lambda_function" "getCameras" {
   filename = "Lambda/CameraLambdas/getCameras.zip"
   function_name = "getCameras"
-  role = "${aws_iam_role.IamForGetVideosLambda.arn}"
+  role = "${aws_iam_role.IamForGetCamerasLambda.arn}"
   handler = "getCameras.handler"
   runtime = "nodejs6.10"
   timeout = "30"
