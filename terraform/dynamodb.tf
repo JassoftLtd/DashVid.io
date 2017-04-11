0ad1c1387606659af55a0f8f9b8e37abc1ef15bb
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-04-11 08:58:08 +0100

    Added cameras table

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -60,3 +60,25 @@ resource "aws_dynamodb_table" "subscriptions-table" {
     projection_type = "ALL"
   }
 }
+
+resource "aws_dynamodb_table" "cameras-table" {
+  name = "Cameras"
+  read_capacity = 1
+  write_capacity = 1
+  hash_key = "Id"
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+  attribute {
+    name = "User"
+    type = "S"
+  }
+  global_secondary_index {
+    name = "UserCameras"
+    hash_key = "User"
+    write_capacity = 1
+    read_capacity = 1
+    projection_type = "ALL"
+  }
+}
