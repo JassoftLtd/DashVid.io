7dcaa754a8cd1df29ac6f406246cf95d6b9e9fe6
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-05-04 14:43:45 +0100

    redirect www. to root

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -10,7 +10,7 @@ resource "aws_cloudfront_distribution" "website_s3_distribution" {
   ]
     origin {
         domain_name = "${aws_s3_bucket.dashvid-io-bucket.bucket}.s3.amazonaws.com"
-        origin_id   = "myS3Origin"
+        origin_id   = "S3Origin"
     }
 
     enabled             = true
@@ -59,4 +59,55 @@ resource "aws_cloudfront_distribution" "website_s3_distribution" {
       response_code = 200
       response_page_path = "/index.html"
     }
+}
+
+resource "aws_cloudfront_distribution" "www-website_s3_distribution" {
+  depends_on = [
+    "aws_s3_bucket.www-dashvid-io-bucket",
+    "aws_cloudfront_origin_access_identity.origin_access_identity"
+  ]
+  origin {
+    domain_name = "${aws_s3_bucket.www-dashvid-io-bucket.bucket}.s3.amazonaws.com"
+    origin_id   = "wwwS3Origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "DashVid.io Website"
+  default_root_object = "index.html"
+  retain_on_delete = true
+
+  aliases = ["www.${var.environment_subdomain}${var.domain_name}"]
+
+  viewer_certificate {
+    acm_certificate_arn = "${var.acm_certificate_arn}"
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "myS3Origin"
+    compress = true
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 86400
+    default_ttl            = 86400
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
 }
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -27,8 +27,8 @@ resource "aws_route53_record" "www_DashCamWeb" {
   name    = "www.${var.environment_subdomain}${var.dns_zone_name}"
   type    = "A"
   alias {
-    name                   = "${aws_s3_bucket.www-dashvid-io-bucket.website_domain}"
-    zone_id                = "${aws_s3_bucket.www-dashvid-io-bucket.hosted_zone_id}"
+    name                   = "${aws_cloudfront_distribution.www-website_s3_distribution.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.www-website_s3_distribution.hosted_zone_id}"
     evaluate_target_health = true
   }
 }
