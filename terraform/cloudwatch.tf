fb7c4da12eee8929d0f2829c291772c51d5a7fbd
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-05-04 17:10:24 +0100

    dynamodb read capacity

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -28,3 +28,20 @@ resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
   alarm_description = "There have been at least 1 Lambda Error(s) in the past 60 seconds"
   alarm_actions       = ["${aws_sns_topic.slack_alert.arn}"]
 }
+
+resource "aws_cloudwatch_metric_alarm" "dynamodb-videos-consumed-read" {
+  alarm_name = "${var.environment_name}dynamodb-videos-consumed-read"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = "1"
+  metric_name = "ConsumedReadCapacityUnits"
+  namespace = "AWS/DynamoDB"
+  period = "60"
+  statistic = "Maximum"
+  threshold = "${aws_dynamodb_table.videos-table.read_capacity * 0.8}"
+  treat_missing_data = "notBreaching"
+  alarm_description = "DynamoDB table ${aws_dynamodb_table.videos-table.name} is consuming 80% of its provisioned read capacity"
+  alarm_actions       = ["${aws_sns_topic.slack_alert.arn}"]
+  dimensions {
+    TableName = "${aws_dynamodb_table.videos-table.name}"
+  }
+}
