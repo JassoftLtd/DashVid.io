516ee86b4e431acb056425095732cd67432e998c
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-09-01 15:38:29 +0100

    Build UI again

--- terraform/build.sh
+++ terraform/build.sh
@@ -8,5 +8,13 @@ go install github.com/jonnyshaw89/terraform-s3-dir
 
 echo "Creating terraform s3 for website"
 
+pushd ../../gh-release-ui
+
+echo "Building UI"
+npm install
+npm run build
+
+popd
+
 websiteBucket=dashvid.io
-$GOPATH/bin/terraform-s3-dir ../../build/ $TF_VAR_environment_name$websiteBucket aws_s3_bucket.dashvid-io-bucket > s3_dashvid-io.tf
+$GOPATH/bin/terraform-s3-dir ../../gh-release-ui/build/ $TF_VAR_environment_name$websiteBucket aws_s3_bucket.dashvid-io-bucket > s3_dashvid-io.tf
