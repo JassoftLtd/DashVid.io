57fe86ac0391a497eceb4222dbf106cc87ac3978
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-10-20 20:49:23 +0100

    Added AWS Batch compute environment

--- terraform/batch.tf
+++ terraform/batch.tf
@@ -1,8 +1,8 @@
 
 resource "aws_batch_compute_environment" "batch_compute" {
-  compute_environment_name = "Dashvid.io"
+  compute_environment_name = "Dashvid"
   compute_resources {
-    bid_percentage = "30%"
+    bid_percentage = "30"
     instance_role = "${aws_iam_instance_profile.batch_compute_ecs_instance_role.arn}"
     instance_type = [
       "c4.xlarge",
