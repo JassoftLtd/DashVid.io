07c239535dfea12ef1e428c04dada5e728f5dce6
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-10-21 17:53:05 +0100

    added awscli

--- docker/transcode/docker-entrypoint.sh
+++ docker/transcode/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Download the Input File
-aws s3 cp s3://$INPUT_BUCKET/$INPUT_FILE inputFile
+aws s3 cp s3://$INPUT_BUCKET/$INPUT_FILE inputFile.mp4
 
 # Transcode the video
-transcode-video --target small --output outputFile inputFile
+transcode-video --target small --output outputFile.m4v inputFile.mp4
 
 # Upload the output to Output bucket
-aws s3 cp outputFile s3://$OUTPUT_BUCKET/$OUTPUT_FILE
+aws s3 cp outputFile.m4v s3://$OUTPUT_BUCKET/$OUTPUT_FILE
