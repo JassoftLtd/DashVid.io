a43a296ee0dfa666739a9493932e292045d0e474
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-10-21 18:46:39 +0100

    added awscli

--- terraform/batch-queue.tf
+++ terraform/batch-queue.tf
@@ -1,9 +1,9 @@
-//resource "aws_batch_job_queue" "video_transcoding_queue" {
-//  name = "${var.environment_name}video_transcoding_queue"
-//  state = "ENABLED"
-//  priority = 1
-//  compute_environments = ["${aws_batch_compute_environment.batch_compute.arn}"]
-//}
+resource "aws_batch_job_queue" "video_transcoding_queue" {
+  name = "${var.environment_name}video_transcoding_queue"
+  state = "ENABLED"
+  priority = 1
+  compute_environments = ["${aws_batch_compute_environment.batch_compute.arn}"]
+}
 
 resource "aws_batch_job_queue" "registration_plate_extraction_queue" {
   name = "${var.environment_name}registration_plate_extraction_queue"
