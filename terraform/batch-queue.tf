5cb687bda71e5661a971c895835f9f82525e637d
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-10-21 12:31:08 +0100

    Added AWS Batch compute queues

--- terraform/batch-queue.tf
+++ terraform/batch-queue.tf
@@ -5,9 +5,9 @@ resource "aws_batch_job_queue" "video_transcoding_queue" {
   compute_environments = ["${aws_batch_compute_environment.batch_compute.arn}"]
 }
 
-resource "aws_batch_job_queue" "registration_plate_extraction_queue" {
-  name = "${var.environment_name}registration_plate_extraction_queue"
-  state = "ENABLED"
-  priority = 2
-  compute_environments = ["${aws_batch_compute_environment.batch_compute.arn}"]
-}
+//resource "aws_batch_job_queue" "registration_plate_extraction_queue" {
+//  name = "${var.environment_name}registration_plate_extraction_queue"
+//  state = "ENABLED"
+//  priority = 2
+//  compute_environments = ["${aws_batch_compute_environment.batch_compute.arn}"]
+//}
