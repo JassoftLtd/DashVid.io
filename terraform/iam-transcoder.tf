b0dccf7c29bf0a94702fca87c12f7ef45abe34c9
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-10-31 09:50:08 +0000

    Added transcode on batch lambda function to trigger

--- terraform/iam-transcoder.tf
+++ terraform/iam-transcoder.tf
@@ -133,6 +133,87 @@ resource "aws_iam_role_policy" "IamForTranscodeVideoLambda" {
   policy = "${data.aws_iam_policy_document.IamForTranscodeVideoLambda.json}"
 }
 
+
+// transcodeVideoOnBatch
+resource "aws_iam_role" "IamForTranscodeVideoOnBatchLambda" {
+  name = "${var.environment_name}iam_for_video_transcoder_batch_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "aws_iam_policy_document" "IamForTranscodeVideoOnBatchLambda" {
+  "statement" = {
+    "effect" = "Allow"
+
+    "actions" = [
+      "dynamodb:GetItem",
+    ]
+
+    "resources" = [
+      "${aws_dynamodb_table.videos-table.arn}",
+    ]
+  }
+
+  "statement" = {
+    "effect" = "Allow"
+
+    "actions" = [
+      "batch:SubmitJob",
+    ]
+
+    "resources" = [
+      "*",
+    ]
+  }
+
+  "statement" = {
+    "effect" = "Allow"
+
+    "actions" = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    "resources" = [
+      "*",
+    ]
+  }
+
+  "statement" = {
+    "effect" = "Allow"
+
+    "actions" = [
+      "xray:PutTraceSegments",
+      "xray:PutTelemetryRecords",
+    ]
+
+    "resources" = [
+      "*",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "IamForTranscodeVideoOnBatchLambda" {
+  name   = "${var.environment_name}IamForTranscodeVideoOnBatchLambda"
+  role   = "${aws_iam_role.IamForTranscodeVideoOnBatchLambda.id}"
+  policy = "${data.aws_iam_policy_document.IamForTranscodeVideoOnBatchLambda.json}"
+}
+
 // videoTranscoded
 resource "aws_iam_role" "IamForVideoTranscodedLambda" {
   name = "${var.environment_name}iam_for_video_transcoded_lambda"
--- terraform/lambda-transcode.tf
+++ terraform/lambda-transcode.tf
@@ -35,6 +35,43 @@ resource "aws_lambda_permission" "transcodeVideo_allow_sns" {
   source_arn    = "${aws_sns_topic.new_video.arn}"
 }
 
+// Transcode Video On AWS Batch
+resource "aws_lambda_function" "transcodeVideoOnBatch" {
+  depends_on = [
+    "aws_iam_role_policy.IamForTranscodeVideoOnBatchLambda",
+  ]
+
+  filename         = "PackagedLambdas/transcodeVideoOnBatch.zip"
+  function_name    = "transcodeVideoOnBatch"
+  role             = "${aws_iam_role.IamForTranscodeVideoOnBatchLambda.arn}"
+  handler          = "transcodeVideoOnBatch.handler"
+  runtime          = "nodejs6.10"
+  timeout          = "30"
+  memory_size      = "256"
+  source_code_hash = "${base64sha256(file("PackagedLambdas/transcodeVideoOnBatch.zip"))}"
+  kms_key_arn      = "${aws_kms_key.lambda_variables.arn}"
+
+  tracing_config {
+    mode = "Active"
+  }
+
+  environment {
+    variables = {
+      jobDefinition = "${aws_batch_job_definition.Transcode.name}"
+      jobName = "Transcode"
+      jobQueue = "${aws_batch_job_queue.video_transcoding_queue.name}"
+    }
+  }
+}
+
+resource "aws_lambda_permission" "transcodeVideoOnBatch_allow_sns" {
+  statement_id  = "AllowExecutionFromSNSForTranscodeVideoOnBatch"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.transcodeVideoOnBatch.arn}"
+  principal     = "sns.amazonaws.com"
+  source_arn    = "${aws_sns_topic.new_video.arn}"
+}
+
 // Video Transcoded
 resource "aws_lambda_function" "videoTranscoded" {
   depends_on = [
