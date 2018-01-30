a1384e49df3a138bc5442fe740fcbde72e656b7a
Jon Shaw <jon.shaw@jassoft.co.uk>
2018-01-30 21:28:43 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@ provider "aws" {
   region     = "${var.aws_region}"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
+  version    = "1.7"
 }
 
 data "aws_caller_identity" "current" {}
