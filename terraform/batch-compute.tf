fe3e995504c2c979edc9da3500729fff245996d4
Jon Shaw <jon.shaw@jassoft.co.uk>
2018-01-30 21:48:18 +0000

    Update batch-compute.tf

--- terraform/batch-compute.tf
+++ terraform/batch-compute.tf
@@ -85,6 +85,13 @@ resource "aws_iam_role_policy_attachment" "ecs_instance_role" {
 resource "aws_security_group" "batch_compute" {
   name   = "aws_batch_compute_environment_security_group"
   vpc_id = "${aws_vpc.batch_compute.id}"
+  
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_vpc" "batch_compute" {
