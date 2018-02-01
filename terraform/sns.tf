1f8a83f01fce76179bfdc4f01b559b0a20cdb823
Jon Shaw <jon.shaw@jassoft.co.uk>
2018-02-01 10:34:59 +0000

    remove sns subscription for transcode

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -12,12 +12,6 @@ resource "aws_sns_topic" "new_video" {
   display_name = "New Video"
 }
 
-resource "aws_sns_topic_subscription" "new_video_lambda_transcode_video" {
-  topic_arn = "${aws_sns_topic.new_video.arn}"
-  protocol  = "lambda"
-  endpoint  = "${aws_lambda_function.transcodeVideo.arn}"
-}
-
 resource "aws_sns_topic_subscription" "new_video_lambda_transcode_on_batch_video" {
   topic_arn = "${aws_sns_topic.new_video.arn}"
   protocol  = "lambda"
